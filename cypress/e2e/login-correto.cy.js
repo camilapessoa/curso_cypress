63b32031efca402d39bf3a4fe8a39d1792dc510d
camilapessoa <camilabentespessoa@gmail.com>
2024-02-10 06:08:24 -0300

    login caminho feliz com beforeEach

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -1,9 +1,25 @@
+// describe('Página de login', () => {
+//   it('Deve preencher os campos do login corretamente e autenticar o usuário na página', () => {
+//     cy.visit('https://adopet-frontend-iota.vercel.app/');
+//     cy.get('[data-test="login-button"]').click();
+//     cy.get('[data-test="input-loginEmail"]').type('marjorie3@email.com');
+//     cy.get('[data-test="input-loginPassword"]').type('Senha123');
+//     cy.get('[data-test="submit-button"]').click();
+//   })
+// })
+
+
+
 describe('Página de login', () => {
-    it('Deve preencher os campos do login corretamente e autenticar o usuário na página', () => {
-      cy.visit('https://adopet-frontend-iota.vercel.app/');
-      cy.get('[data-test="login-button"]').click();
-      cy.get('[data-test="input-loginEmail"]').type('marjorie3@email.com');
-      cy.get('[data-test="input-loginPassword"]').type('Senha123');
-      cy.get('[data-test="submit-button"]').click();
-    })
+
+  beforeEach(() => {
+    cy.visit('https://adopet-frontend-iota.vercel.app/');
+    cy.get('[data-test="login-button"]').click();
+
   })
+      it('Deve preencher os campos do login corretamente e autenticar o usuário na página', () => {
+        cy.get('[data-test="input-loginEmail"]').type('marjorie3@email.com');
+        cy.get('[data-test="input-loginPassword"]').type('Senha123');
+        cy.get('[data-test="submit-button"]').click();
+      })
+})
