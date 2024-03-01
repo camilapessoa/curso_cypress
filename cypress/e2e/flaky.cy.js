849f3573b8dde5923159445f02c0dbef244003e9
Camila <camilabentespessoa@gmail.com.br>
2024-03-01 13:54:13 -0300

    flaky 2

--- cypress/e2e/flaky.cy.js
+++ cypress/e2e/flaky.cy.js
@@ -1,20 +1,22 @@
 describe('Teste Flaky - Carregamento da Página', () => {
+  
   it('deve carregar a página corretamente', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
 
     // Verifica se o título da página é o esperado
-    cy.title().should('eq', 'AdoPet');
+    // cy.title().should('eq', 'AdoPet');
 
     // Verifica se o logo da Adote um Pet está presente
-    cy.get('.initial > img').should('be.visible');
+    // cy.get('.initial > img').should('be.visible');
     // cy.get('.home__buttons').click()
-    cy.contains('a', 'Ver pets disponíveis para adoção').should('be.visible')
+    // cy.contains('a', 'Ver pets disponíveis para adoção').should('be.visible')
     cy.get('[data-test="login-button"]').click();
         
     cy.login('camila@exemplo.com', 'Senha123');
-    cy.wait(Math.random() * 50); 
-    cy.get('.cards').should('be.visible')
+    
+     cy.get('.cards').should('be.visible')
     // cy.get(':nth-child(7) > img').should('be.visible')
+    
   
   });
   
